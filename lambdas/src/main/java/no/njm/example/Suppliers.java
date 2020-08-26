6713d14e75dc061b2cad6ea88c22df32d4dd6d97
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 10:07:24 +0300

    lombok added, sonarLint issues fixed

--- lambdas/src/main/java/no/njm/example/Suppliers.java
+++ lambdas/src/main/java/no/njm/example/Suppliers.java
@@ -1,16 +1,15 @@
 package no.njm.example;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.function.Supplier;
 
 /**
  * Suppliers produce a result of a given generic type. Unlike Functions, Suppliers don't accept arguments.
  */
+@Slf4j
 public class Suppliers {
-
-    private static final Logger log = LoggerFactory.getLogger(Functions.class);
 
     public static void main(String[] args) {
         Supplier<Person> personSupplier = Person::new;
@@ -19,16 +18,8 @@ public class Suppliers {
         log.debug("Person has id {}", person.getId());
     }
 
+    @Data
     private static class Person {
-
         private int id;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
     }
 }
