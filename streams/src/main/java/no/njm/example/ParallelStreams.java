3ab2a116677d0b796973c27d16c7c6221a807dca
r84160880 <ramazan.sakin@huawei.com>
2020-08-26 08:35:17 +0300

    Refactoring

--- streams/src/main/java/no/njm/example/ParallelStreams.java
+++ streams/src/main/java/no/njm/example/ParallelStreams.java
@@ -16,37 +16,32 @@ public class ParallelStreams {
     private static final int MAX = 1000;
 
     public static void main(String[] args) {
-        sortSequential();
-        sortParallel();
+        // sequentialSort - mode : 1
+        sort(1);
+        // parallelSort - mode : 1
+        sort(2);
     }
 
-    private static void sortSequential() {
-        List<String> list = new ArrayList<>(MAX);
-        for (int i = 0; i < MAX; i++) {
-            UUID uuid = UUID.randomUUID();
-            list.add(uuid.toString());
-        }
+    private static void sort(int mode) {
+        List<String> list = getSampleStrings();
 
         long start = System.nanoTime();
-        list.stream()
-                .sorted()
-                .count();
+        long count;
+        if (mode == 1)
+            count = list.stream().count();
+        else
+            count = list.parallelStream().count();
         long end = System.nanoTime();
+        log.info("Count : " + count);
         log.debug(String.format("Sequential sort took: %d ms", TimeUnit.NANOSECONDS.toMillis(end - start)));
     }
 
-    private static void sortParallel() {
+    private static List<String> getSampleStrings() {
         List<String> list = new ArrayList<>(MAX);
         for (int i = 0; i < MAX; i++) {
             UUID uuid = UUID.randomUUID();
             list.add(uuid.toString());
         }
-
-        long start = System.nanoTime();
-        list.parallelStream()
-                .sorted()
-                .count();
-        long end = System.nanoTime();
-        log.debug(String.format("Parallel sort took: %d ms", TimeUnit.NANOSECONDS.toMillis(end - start)));
+        return list;
     }
 }
