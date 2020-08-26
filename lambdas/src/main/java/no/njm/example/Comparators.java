f73428d20492843777e8223901a8b7dc68153397
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 09:02:15 +0300

    Comparators logger & sonarLint issue fix

--- lambdas/src/main/java/no/njm/example/Comparators.java
+++ lambdas/src/main/java/no/njm/example/Comparators.java
@@ -1,28 +1,26 @@
 package no.njm.example;
 
-import java.util.Comparator;
+import lombok.extern.slf4j.Slf4j;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Comparator;
 
 /**
  * Comparators are well known from older versions of Java. Java 8 adds various default methods to the interface
  */
+@Slf4j
 public class Comparators {
-
-    private static final Logger log = LoggerFactory.getLogger(Functions.class);
 
     public static void main(String[] args) {
         Person person1 = new Person("First", "Last");
         Person person2 = new Person("Second", "Third");
 
-        Comparator<Person> firstNameComparator = (p1, p2) -> p1.firstName.compareTo(p2.firstName);
+        Comparator<Person> firstNameComparator = Comparator.comparing(p -> p.firstName);
         log.debug("Comparing firstname returns {}", firstNameComparator.compare(person1, person2));
         log.debug("Comparing lastname reversed returns {}", firstNameComparator.reversed().compare(person1, person2));
 
         // Chaining comparators
         log.debug("Comparing firstname and lastname returns {}", firstNameComparator
-                .thenComparing((p1, p2) -> p1.lastName.compareTo(p2.lastName))
+                .thenComparing(p -> p.lastName)
                 .compare(person1, person2));
     }
 }
