d9f19a5d537e811e4f7b560d4399effaede361b7
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-26 09:03:38 +0300

    Consumers logger & sonarLint issue fix

--- lambdas/src/main/java/no/njm/example/Consumers.java
+++ lambdas/src/main/java/no/njm/example/Consumers.java
@@ -1,19 +1,22 @@
 package no.njm.example;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.Consumer;
 
 /**
  * Consumers represents operations to be performed on a single input argument.
  */
+@Slf4j
 public class Consumers {
 
     public static void main(String[] args) {
         Person person = new Person("First", "Last");
-        Consumer<Person> printFirstName = (p) -> System.out.println(p.firstName);
+        Consumer<Person> printFirstName = p -> log.info(p.firstName);
         printFirstName.accept(person);
 
         // Chaining consumers
-        Consumer<Person> printName = printFirstName.andThen((p) -> System.out.println(p.lastName));
+        Consumer<Person> printName = printFirstName.andThen(p -> log.info(p.lastName));
         printName.accept(person);
     }
 }
